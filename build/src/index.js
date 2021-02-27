07e9e7cfcd0d4c83dd6955b529e3b1588a8ff6f1
RoTour <rothewawa@gmail.com>
2021-02-27 18:51:46 +0100

    reset to default connection - using heroku env var

--- build/src/index.js
+++ build/src/index.js
@@ -13,10 +13,31 @@ require("reflect-metadata");
 const typeorm_1 = require("typeorm");
 const application_config_1 = require("./config/application.config");
 const server_1 = require("./server");
+// const getOptions = async () => {
+//   let connectionOptions: ConnectionOptions;
+//   connectionOptions = {
+//     type: 'postgres',
+//     synchronize: true,
+//     logging: false,
+//     entities: ['**/models/**/*.entity.ts', '**/models/**/*.entity.js'],
+//   };
+//   if (process.env.DATABASE_URL) {
+//     Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
+//   } else {
+//     // gets your default configuration
+//     // you could get a specific config by name getConnectionOptions('production')
+//     // or getConnectionOptions(process.env.NODE_ENV)
+//     connectionOptions = await getConnectionOptions();
+//   }
+//
+//   return connectionOptions;
+// };
 (() => __awaiter(void 0, void 0, void 0, function* () {
     // eslint-disable-next-line no-console
     console.clear();
     const config = application_config_1.getApplicationConfig();
+    // const typeormConfig = await getOptions();
+    // await createConnection(typeormConfig);
     yield typeorm_1.createConnection();
     // Start the server
     yield server_1.startServer(config);
