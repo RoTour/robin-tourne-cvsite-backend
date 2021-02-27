5d4565da46f41fc81e414dced99bcb695d17613a
RoTour <rothewawa@gmail.com>
2021-02-27 18:37:24 +0100

    changed db connection options

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
 import 'reflect-metadata';
-import { createConnection } from 'typeorm';
+import { ConnectionOptions, createConnection, getConnectionOptions } from 'typeorm';
 import { getApplicationConfig } from './config/application.config';
 import { startServer } from './server';
+
+const getOptions = async () => {
+  let connectionOptions: ConnectionOptions;
+  connectionOptions = {
+    type: 'postgres',
+    synchronize: true,
+    logging: false,
+    extra: {
+      ssl: true,
+    },
+    entities: ['**/models/**/*.entity.ts', '**/models/**/*.entity.js'],
+  };
+  if (process.env.DATABASE_URL) {
+    Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
+  } else {
+    // gets your default configuration
+    // you could get a specific config by name getConnectionOptions('production')
+    // or getConnectionOptions(process.env.NODE_ENV)
+    connectionOptions = await getConnectionOptions();
+  }
+
+  return connectionOptions;
+};
 
 (async () => {
   // eslint-disable-next-line no-console
@@ -9,7 +32,8 @@ import { startServer } from './server';
 
   const config = getApplicationConfig();
 
-  await createConnection();
+  const typeormConfig = await getOptions();
+  await createConnection(typeormConfig);
 
   // Start the server
   await startServer(config);
