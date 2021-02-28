307a5571a4e68d5ed180fcd747a66f8fcc2ff5f3
RoTour <rothewawa@gmail.com>
2021-02-28 09:28:30 +0100

    trying using DATABASE_URL

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,41 @@
 import 'reflect-metadata';
-import { createConnection } from 'typeorm';
+import { ConnectionOptions, createConnection, getConnectionOptions } from 'typeorm';
+import dotenv from 'dotenv';
 import { getApplicationConfig } from './config/application.config';
 import { startServer } from './server';
 
-// const getOptions = async () => {
-//   let connectionOptions: ConnectionOptions;
-//   connectionOptions = {
-//     type: 'postgres',
-//     synchronize: true,
-//     logging: false,
-//     entities: ['**/models/**/*.entity.ts', '**/models/**/*.entity.js'],
-//   };
-//   if (process.env.DATABASE_URL) {
-//     Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
-//   } else {
-//     // gets your default configuration
-//     // you could get a specific config by name getConnectionOptions('production')
-//     // or getConnectionOptions(process.env.NODE_ENV)
-//     connectionOptions = await getConnectionOptions();
-//   }
-//
-//   return connectionOptions;
-// };
+dotenv.config();
+
+const getOptions = async () => {
+  let connectionOptions: ConnectionOptions;
+  connectionOptions = {
+    type: 'postgres',
+    synchronize: true,
+    logging: false,
+    extra: {
+      ssl: true,
+    },
+    entities: [
+      '**/models/**/*.entity.ts',
+      '**/models/**/*.entity.js',
+    ],
+  };
+  if (process.env.DATABASE_URL) {
+    Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
+  } else {
+    // gets your default configuration
+    // you could get a specific config by name getConnectionOptions('production')
+    // or getConnectionOptions(process.env.NODE_ENV)
+    connectionOptions = await getConnectionOptions();
+  }
+
+  return connectionOptions;
+};
+
+const connect2Database = async (): Promise<void> => {
+  const typeormConfig = await getOptions();
+  await createConnection(typeormConfig);
+};
 
 (async () => {
   // eslint-disable-next-line no-console
@@ -32,7 +46,7 @@ import { startServer } from './server';
   // const typeormConfig = await getOptions();
   // await createConnection(typeormConfig);
 
-  await createConnection();
+  await connect2Database();
 
   // Start the server
   await startServer(config);
